46051567996112136497bfd8dbd4771ffd58d563
frederick-bruce <frederick.bruce.dev@gmail.com>
2023-08-03 14:00:30 -0400

    file movement
    moved index.ts to src

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,15 @@
-import app from ".";
+import app from "./src";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
 const port = env.PORT;
 
-mongoose.connect(env.MONGO_CONNECTION_STRING)
-    .then(() => {
-        console.log("Mongoose connected");
-        app.listen(port, () => {
-            console.log("Server running on port: " + port);
-        });
-    })
-    .catch(console.error);
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .catch(console.error);
